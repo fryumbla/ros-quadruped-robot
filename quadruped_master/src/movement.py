e167b38c2f4aea9014b1e43968b6cbdca9d65d33
Francisco Yumbla <fryumbl@espol.edu.ec>
2020-08-29 21:19:43 +0900

    update

--- quadruped_master/src/movement.py
+++ quadruped_master/src/movement.py
@@ -5,54 +5,154 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 
 def main():
-
-
     rospy.init_node("stand_up")
+    pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+    joints_states = JointState()
+
+    stand50j14=0.41944732836554044
+    stand50j58=1.719784407902978
+
+    stand65j14=0.7592545338404827
+    stand65j58=1.169485889801056
 
     while not rospy.is_shutdown():
+
+        joints_states.header = Header()
+        joints_states.header.stamp = rospy.Time.now()
+        joints_states.name = ['front_right_joint1', 'front_right_joint2', 'front_left_joint1','front_left_joint2', 'back_left_joint1', 'back_left_joint2', 'back_right_joint1','back_right_joint2']
+        
+        time=1
+        rate = rospy.Rate(10) # 10hz  
              
         # joint_position_state=[-1,2,-1,2,-1,2,-1,2]
         # joint_position_state=[0,0,0,0,0,0,0,0]
         number = input ("Enter number: ")
         if (number==1):
             joint_position_state=[0,0,0,0,0,0,0,0]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==2):
-            print("dos")
+            # up
             joint_position_state=[-1,2.2,-1,2.2,-1,2.2,-1,2.2]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58] # stand up principal
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==3):
-            #parado
-            joint_position_state=[0.759,1.16937,0.759,1.16937,0.759,1.16937,0.759,1.16937]
+            # down
+            joint_position_state=[-1,2.2,-1,2.2,-1,2.2,-1,2.2]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[-1,0,-1,0,-1,0,-1,0]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[0,0,0,0,0,0,0,0]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==4):
-            #derecha alza
-            joint_position_state=[0.4194,1.7198,0.759,1.16937,0.4194,1.7198,0.759,1.16937]		
+            joint_position_state=[stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==5):
-            # primer paso
-            joint_position_state=[0.7047391,1.009487,0.759,1.16937,0.9484119,1.244768,0.759,1.16937]
+            joint_position_state=[stand50j14,stand50j58,stand50j14,stand50j58,stand50j14,stand50j58,stand50j14,stand50j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==6):
-            joint_position_state=[0.759,1.16937,0.4194,1.7198,0.759,1.16937,0.4194,1.7198]	
+            #foward 
+            joint_position_state=[0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58]	
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)	
+            rospy.sleep(time)
+            joint_position_state=[0.7047391,1.009487,stand65j14,stand65j58,0.9484119,1.244768,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,0.7047391,1.009487,stand65j14,stand65j58,0.9484119,1.244768]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==7):
-            # segundo paso
-            joint_position_state=[0.759,1.16937,0.7047391,1.009487,0.759,1.16937,0.9484119,1.244768]
+            # backward
+            joint_position_state=[0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58]	
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)	
+            rospy.sleep(time)
+            joint_position_state=[stand65j14+0.0542609,stand65j58+0.159883,stand65j14,stand65j58,stand65j14-0.1894119,stand65j58-0.075398,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,stand65j14+0.0542609,stand65j58+0.159883,stand65j14,stand65j58,stand65j14-0.1894119,stand65j58-0.075398]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+
+
+
         if (number==8):
             # segundo paso
-            joint_position_state=[0,1.16937,0,1.16937,0,1.16937,0,1.16937]
+            joint_position_state=[0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58]	
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)	
+            rospy.sleep(time)
+            joint_position_state=[0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,0.7047391,1.009487,stand65j14,stand65j58,0.9484119,1.244768]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
         if (number==9):
             # segundo paso
-            joint_position_state=[-1,0,-1,0,-1,0,-1,0]
+            joint_position_state=[0.4194,1.7198,stand65j14,stand65j58,0.4194,1.7198,stand65j14,stand65j58]	
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)	
+            rospy.sleep(time)
+            joint_position_state=[-0.785398,2.6179,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[-0.785398,0,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[0,0,stand65j14,stand65j58,stand65j14,stand65j58,stand65j14,stand65j58]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            rospy.sleep(time)
+            joint_position_state=[0,0,stand65j14+0.1745329,stand65j58+0.1745329,stand65j14-0.1745329,stand65j58-0.1745329,stand65j14-0.1745329,stand65j58-0.1745329]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            joint_position_state=[0,0,stand65j14+0.1745329,stand65j58+0.1745329,stand65j14-0.1745329,stand65j58-0.1745329,stand65j14-0.1745329,stand65j58-0.1745329]
+            joints_states.position = joint_position_state
+            pub.publish(joints_states)
+            # rospy.sleep(time)
                     
-        pub = rospy.Publisher('joint_states', JointState, queue_size=10)
-        # rate = rospy.Rate(1000000) # 10hz
-        joints_states = JointState()
-        joints_states.header = Header()
-        joints_states.header.stamp = rospy.Time.now()
-        joints_states.name = ['front_right_joint1', 'front_right_joint2', 'front_left_joint1','front_left_joint2', 'back_left_joint1', 'back_left_joint2', 'back_right_joint1','back_right_joint2']
-        joints_states.position = joint_position_state
-        pub.publish(joints_states)
-        rate = rospy.Rate(10) # 10hz   
-        # rospy.sleep(5)
-        # joints_states.name = ['front_right_joint1', 'front_right_joint2', 'front_left_joint1','front_left_joint2', 'back_left_joint1', 'back_left_joint2', 'back_right_joint1','back_right_joint2']
-        # joints_states.position = [0,2,0,2,0,2,0,2]
-        # pub.publish(JointState)
 
 
 if __name__ == '__main__':
--- simulation_graphs/quadrupedwalking.py
+++ simulation_graphs/quadrupedwalking.py
@@ -4,10 +4,10 @@ import matplotlib.animation as animation
 import math
 
 ######################### you can change these values ########################
-link1 = 0.3  # link length setting
-link2 = 0.3
-th1 = 45.0
-th2 = 45.0
+link1 = 0.4  # link length setting
+link2 = 0.4
+# th1 = 45.0
+# th2 = 45.0
 ##############################################################################
 
 # value initialize
@@ -17,26 +17,27 @@ fl = []
 br = []
 bl = []
 
+stand=0.65
+
 # control loop
-step = 1000
-rfjoint1=[0.0,0.5]
-rbjoint1=[-0.75,0.5]
+step = 20
+walk_width = 0.15
+rfjoint1=[0.11,stand]
+rbjoint1=[-0.11,stand]
 
-# f= open("/home/wsj/python_coding/quadruped/file/angle.txt",'w')
-
+# comienzo de la caminata primera position
 def beginhold(trajectory, walkwidth,front,i):
 
     theta = math.pi + i*math.pi/(step-1)
-    x = 0
-    y = 0.5
+    x = 0.15
+    y = stand
 
     th2 = np.arccos(((x**2)+(y**2)-(link1**2)-(link2**2))/(2*link1*link2))
     th2_i = -th2
-    th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2) -
-                                      (link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
+    th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2)-(link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
 
-    # f.write("%f ," %th1)
-    # f.write("%f \n" %th2)
+    print("joint1: ", th1, " Joint2: ", th2)
+
     
     if front==True:
         joint1 = rfjoint1
@@ -50,21 +51,23 @@ def beginhold(trajectory, walkwidth,front,i):
         joint3 = [joint2[0]-link2*np.cos(th1+th2), joint2[1]-link2*np.sin(th1+th2)]
 
         trajectory.append([joint1, joint2, joint3])
+
     return joint1
 
+# comienzo desde atras
 def beginstepforward(trajectory, walkwidth,front,i):
 
     theta = math.pi + i*math.pi/(step-1)
-    x = -walkwidth/2.0- (walkwidth/2.0 * np.cos(theta))
-    y = 0.5 + walkwidth/2.0 * np.sin(theta)
+    x = walkwidth/2.0- (walkwidth/2.0 * np.cos(theta))
+    y = stand + walkwidth/2.0 * np.sin(theta)
 
     th2 = np.arccos(((x**2)+(y**2)-(link1**2)-(link2**2))/(2*link1*link2))
     th2_i = -th2
     th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2) -
                                       (link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
 
-    # f.write("%f ," %th1)
-    # f.write("%f \n" %th2)
+    print("joint1: ", th1, " Joint2: ", th2)
+
     if front==True:
         joint1 = fjoint1
         joint2 = [joint1[0]-link1*np.cos(th1), joint1[1]-link1*np.sin(th1)]
@@ -78,20 +81,97 @@ def beginstepforward(trajectory, walkwidth,front,i):
 
         trajectory.append([joint1, joint2, joint3])
 
+
+# paso atras
 def pullback(trajectory, walkwidth,front,i):
 
-    x = -walkwidth+(i*(walkwidth*2/(step-1)))
-    y = 0.5
-    xi = -walkwidth
-    yi = 0.5
+    x = 0.15-walkwidth+(i*(walkwidth/(step-1)))
+    y = stand
+    xi = 0.15-walkwidth
+    yi = stand
 
     th2 = np.arccos(((x**2)+(y**2)-(link1**2)-(link2**2))/(2*link1*link2))
     th2_i = -th2
     th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2) -
                                       (link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
 
-    # f.write("%f ," %th1)
-    # f.write("%f \n" %th2)
+    print("joint1: ", th1, " Joint2: ", th2)
+
+    th2i = np.arccos(((xi**2)+(yi**2)-(link1**2)-(link2**2))/(2*link1*link2))
+    th1i = np.arctan2(yi, xi)-np.arccos(((xi**2)+(yi**2)+(link1**2) -
+                                      (link2**2)) / (2*link1*math.sqrt(xi**2+yi**2)))
+    if front==True:
+        joint1 = rfjoint1
+        joint2 = [joint1[0]-link1*np.cos(th1i), joint1[1]-link1*np.sin(th1i)]
+        joint3 = [joint2[0]-link2*np.cos(th1i+th2i), joint2[1]-link2*np.sin(th1i+th2i)]
+
+        joint2 = [joint3[0]+link2*np.cos(th1+th2), joint3[1]+link2*np.sin(th1+th2)]
+        joint1 = [joint2[0]+link1*np.cos(th1), joint2[1]+link1*np.sin(th1)]
+
+        trajectory.append([joint1, joint2, joint3])
+    else:
+        joint1 = rbjoint1
+        joint2 = [joint1[0]-link1*np.cos(th1i), joint1[1]-link1*np.sin(th1i)]
+        joint3 = [joint2[0]-link2*np.cos(th1i+th2i), joint2[1]-link2*np.sin(th1i+th2i)]
+
+        joint2 = [joint3[0]+link2*np.cos(th1+th2), joint3[1]+link2*np.sin(th1+th2)]
+        joint1 = [joint2[0]+link1*np.cos(th1), joint2[1]+link1*np.sin(th1)]
+
+        trajectory.append([joint1, joint2, joint3])
+    return joint1
+
+# paso atras
+def pullbackfoward(trajectory, walkwidth,front,i):
+
+    x = -walkwidth+(i*(walkwidth/(step-1)))
+    y = stand
+    xi = -walkwidth
+    yi = stand
+
+    th2 = np.arccos(((x**2)+(y**2)-(link1**2)-(link2**2))/(2*link1*link2))
+    th2_i = -th2
+    th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2) -
+                                      (link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
+
+    print("joint1: ", th1, " Joint2: ", th2)
+
+    th2i = np.arccos(((xi**2)+(yi**2)-(link1**2)-(link2**2))/(2*link1*link2))
+    th1i = np.arctan2(yi, xi)-np.arccos(((xi**2)+(yi**2)+(link1**2) -
+                                      (link2**2)) / (2*link1*math.sqrt(xi**2+yi**2)))
+    if front==True:
+        joint1 = rfjoint1
+        joint2 = [joint1[0]-link1*np.cos(th1i), joint1[1]-link1*np.sin(th1i)]
+        joint3 = [joint2[0]-link2*np.cos(th1i+th2i), joint2[1]-link2*np.sin(th1i+th2i)]
+
+        joint2 = [joint3[0]+link2*np.cos(th1+th2), joint3[1]+link2*np.sin(th1+th2)]
+        joint1 = [joint2[0]+link1*np.cos(th1), joint2[1]+link1*np.sin(th1)]
+
+        trajectory.append([joint1, joint2, joint3])
+    else:
+        joint1 = rbjoint1
+        joint2 = [joint1[0]-link1*np.cos(th1i), joint1[1]-link1*np.sin(th1i)]
+        joint3 = [joint2[0]-link2*np.cos(th1i+th2i), joint2[1]-link2*np.sin(th1i+th2i)]
+
+        joint2 = [joint3[0]+link2*np.cos(th1+th2), joint3[1]+link2*np.sin(th1+th2)]
+        joint1 = [joint2[0]+link1*np.cos(th1), joint2[1]+link1*np.sin(th1)]
+
+        trajectory.append([joint1, joint2, joint3])
+    return joint1
+
+# paso atras
+def pullbackbackward(trajectory, walkwidth,front,i):
+
+    x = 0.15-walkwidth+(i*(walkwidth/(step-1)))
+    y = stand
+    xi = -walkwidth
+    yi = stand
+
+    th2 = np.arccos(((x**2)+(y**2)-(link1**2)-(link2**2))/(2*link1*link2))
+    th2_i = -th2
+    th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2) -
+                                      (link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
+
+    print("joint1: ", th1, " Joint2: ", th2)
 
     th2i = np.arccos(((xi**2)+(yi**2)-(link1**2)-(link2**2))/(2*link1*link2))
     th1i = np.arctan2(yi, xi)-np.arccos(((xi**2)+(yi**2)+(link1**2) -
@@ -118,17 +198,16 @@ def pullback(trajectory, walkwidth,front,i):
 
 def stepforward(trajectory, walkwidth,front,i):
 
-    theta = math.pi + i*math.pi/(step-1)
+    theta = math.pi + (i*math.pi/(step-1))
     x = - walkwidth * np.cos(theta)
-    y = 0.5 + walkwidth * np.sin(theta)
+    y = stand + walkwidth * np.sin(theta)
 
     th2 = np.arccos(((x**2)+(y**2)-(link1**2)-(link2**2))/(2*link1*link2))
     th2_i = -th2
     th1 = np.arctan2(y, x)-np.arccos(((x**2)+(y**2)+(link1**2) -
                                       (link2**2)) / (2*link1*math.sqrt(x**2+y**2)))
 
-    # f.write("%f ," %th1)
-    # f.write("%f \n" %th2)
+    print("joint1: ", th1, " Joint2: ", th2)
 
     if front==True:
         joint1 = fjoint1
@@ -142,36 +221,47 @@ def stepforward(trajectory, walkwidth,front,i):
         joint3 = [joint2[0]-link2*np.cos(th1+th2), joint2[1]-link2*np.sin(th1+th2)]
 
         trajectory.append([joint1, joint2, joint3])
+print("\nMovement 1")
+for i in range(0, step):
+    fjoint1 = beginhold(fl,walk_width,True,i)
+    beginstepforward(fr,walk_width,True,i)
+    bjoint1 = beginhold(br,walk_width,False,i)
+    beginstepforward(bl,walk_width,False,i)
+rfjoint1=fjoint1
+rbjoint1=bjoint1
+print("\nMovement 2")
+for i in range(0, step):
+    fjoint1 = pullback(fr, walk_width,True,i)
+    beginstepforward(fl,walk_width,True,i)   
+    bjoint1 = pullback(bl, walk_width,False,i)
+    beginstepforward(br,walk_width,False,i)   
+rfjoint1=fjoint1
+rbjoint1=bjoint1
+print("\nMovement 3")
+for i in range(0, step):
+    fjoint1 = pullback(fl, walk_width,True,i)
+    stepforward(fr, walk_width,True,i)
+    bjoint1 = pullback(br, walk_width,False,i)
+    stepforward(bl, walk_width,False,i)
+rfjoint1=fjoint1
+rbjoint1=bjoint1
+print("\nMovement 4")
+for i in range(0, step):
+    fjoint1 = pullbackfoward(fr, walk_width,True,i)
+    stepforward(fl, walk_width,True,i)
+    bjoint1 = pullbackfoward(bl, walk_width,False,i)
+    stepforward(br, walk_width,False,i)
+rfjoint1=fjoint1
+rbjoint1=bjoint1
+print("\nMovement 5")
+for i in range(0, step):
+    fjoint1 = pullbackbackward(fl, walk_width,True,i)
+    stepforward(fr, walk_width,True,i)  
+    bjoint1 = pullbackbackward(br, walk_width,False,i)
+    stepforward(bl, walk_width,False,i)  
+rfjoint1=fjoint1
+rbjoint1=bjoint1
 
-for i in range(0, step):
-    fjoint1 = beginhold(fl,0.15,True,i)
-    beginstepforward(fr,0.15,True,i)
-    bjoint1 = beginhold(br,0.15,False,i)
-    beginstepforward(bl,0.15,False,i)
-rfjoint1=fjoint1
-rbjoint1=bjoint1
-for i in range(0, step):
-    fjoint1 = pullback(fr, 0.15,True,i)
-    beginstepforward(fl,0.15,True,i)   
-    bjoint1 = pullback(bl, 0.15,False,i)
-    beginstepforward(br,0.15,False,i)   
-rfjoint1=fjoint1
-rbjoint1=bjoint1
-for i in range(0, step):
-    fjoint1 = pullback(fl, 0.15,True,i)
-    stepforward(fr, 0.15,True,i)
-    bjoint1 = pullback(br, 0.15,False,i)
-    stepforward(bl, 0.15,False,i)
-rfjoint1=fjoint1
-rbjoint1=bjoint1
-for i in range(0, step):
-    fjoint1 = pullback(fr, 0.15,True,i)
-    stepforward(fl, 0.15,True,i)
-    bjoint1 = pullback(bl, 0.15,False,i)
-    stepforward(br, 0.15,False,i)
-
-# f.close()
-print fl[1][0][0]
 # # create a time array from 0..100 sampled at 0.05 second steps
 dt = 0.05
 
@@ -179,11 +269,11 @@ fig = plt.figure(figsize=(16, 8))
 ax = fig.add_subplot(111, autoscale_on=False, xlim=(-1, 3), ylim=(-0.5, 1.5))
 ax.grid()
 
-line1, = ax.plot([], [], 'o-', lw=2)
-line2, = ax.plot([], [], 'o-', lw=2)
-line3, = ax.plot([], [], 'o-', lw=2)
-line4, = ax.plot([], [], 'o-', lw=2)
-line5, = ax.plot([], [], 'o-', lw=2)
+line1, = ax.plot([], [], 'o-', color='red'      , lw=2)
+line2, = ax.plot([], [], 'o-', color='green'    , lw=2)
+line3, = ax.plot([], [], 'o-', color='cyan'     , lw=2)
+line4, = ax.plot([], [], 'o-', color='yellow'   , lw=2)
+line5, = ax.plot([], [], 'o-', color='black'    , lw=2) #line body
 
 def init():
     line1.set_data([], [])
@@ -222,8 +312,7 @@ def animate(i):
     return line1,line2,line3,line4,line5
 
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(fr)),
-                              interval=1, blit=True, init_func=init, repeat=False)
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(fr)),interval=1, blit=True, init_func=init, repeat=True)
 
 # ani.save('double_pendulum.mp4', fps=15)
 plt.show()
